49495c878259c53237f8c83276bc7859821242db
Misha <mihach2k17@gmail.com>
2026-04-03 17:58:48 +0300

    first commit

---
+++ BrokerGame.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+/*
+TODO: 123
+ * Создать список валют
+ * Создать генератор валют
+*/
+
+void draw_graph() {}
+
+class Currency {
+private:
+  string name;
+  int price;
+  vector<int> schanges;
+
+public:
+  Currency(string _name, int _price, vector<int> &_schanges) {
+    name = _name;
+    price = _price;
+    schanges = _schanges;
+  }
+  void priceChange() {}
+};
+
+struct Balance {
+  int price;
+  int count;
+};
+
+class Terminal {
+protected:
+  vector<Currency> currencies;
+
+public:
+  void show_currencies() {}
+};
+
+class Broker {
+private:
+  map<Currency, Balance> portfolio;
+
+public:
+  void show_portfolio() {}
+  void buy() {}
+  void sell() {}
+};
+
+int main() { std::cout << "Hello World!\n"; }
